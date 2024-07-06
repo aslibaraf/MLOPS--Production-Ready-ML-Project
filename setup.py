016f19fb60e9ddd2b04511f897c8cdbf00166b23
aslibaraf <himalayparmar6.com>
2024-07-06 11:43:45 +0530

    setup file changes

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Himalay P Parmar",
+    author_email="aslibaraf@gmail.com",
+    packages=find_packages()
+)
